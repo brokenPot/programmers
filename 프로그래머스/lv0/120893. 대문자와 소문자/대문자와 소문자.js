da5863b449cd2ee81a91eab13c881ad5461111cb
Seo Jeong Woo <64897060+teapotsoup@users.noreply.github.com>
2022-11-06 20:10:53 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.29 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    var answer = '';
+      for (let i = 0; i < my_string.length; ++i) {
+    my_string[i] === my_string[i].toUpperCase()
+      ? (answer += my_string[i].toLowerCase())
+      : (answer += my_string[i].toUpperCase());
+  }
+    return answer;
+}
