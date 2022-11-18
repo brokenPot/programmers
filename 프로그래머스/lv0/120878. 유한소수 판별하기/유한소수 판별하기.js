e11e9e18f02b6239d511277f1be5830bdc16b69f
Seo Jeong Woo <64897060+teapotsoup@users.noreply.github.com>
2022-11-18 20:04:45 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,23 +1,40 @@
-function solution(a, b) {
-  const getGCD = (a, b) => {
-    let gcd = 1;
-    for (let i = 1; i <= Math.min(a, b); i++) {
-      if (a % i === 0 && b % i === 0) gcd = i;
+let getGCD = (num1, num2) => {
+  let gcd = 1;
+  for (let i = 2; i <= Math.min(num1, num2); i++) {
+    if (num1 % i === 0 && num2 % i === 0) {
+      gcd = i;
     }
-    return gcd;
-  };
+  }
+  return gcd;
+};
 
-  const getPrimeFactors = num => {
-    let pFactors = [];
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-      while (num % i === 0) {
-        pFactors = [...pFactors, i];
-        num /= i;
+const primeFactors = (n) => {
+  while (n % 2 === 0) {
+    n /= 2;
+  }
+  for (let i = 3; i * i <= n; i += 2) {
+    while (n % i === 0) {
+      if (i !== 5) {
+        return 2;
       }
+      n /= i;
     }
-    if (num > 2) pFactors = [...pFactors, num];
-    return pFactors;
-  };
+  }
+  if (n > 2) {
+    if (n !== 5) {
+      return 2;
+    }
+  }
+  return 1;
+};
 
-  return getPrimeFactors(b / getGCD(a, b)).find(n => n !== 2 && n !== 5) ? 2 : 1;
+function solution(a, b) {
+  const gcd = getGCD(a, b);
+  const minSon = a / gcd;
+  const minMom = b / gcd;
+  if (Number.isInteger(minSon / minMom)) {
+    return 1;
+  }
+  let tf = primeFactors(minMom);
+  return tf;
 }
