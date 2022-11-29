03f25c18ea7c2feb199fee98e4474295286b439a
Seo Jeong Woo <64897060+teapotsoup@users.noreply.github.com>
2022-11-29 21:33:46 +0900

    [level 2] Title: 카펫, Time: 3.00 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,13 +1,14 @@
 function solution(brown, yellow) {
-  let sum = brown + yellow;
-
-  //카펫의 최소높이는 3부터이다.(위아래 갈색, 가운데가 노란색이기 때문에)
-  for (let height = 3; height <= brown; height++) {
-    if (sum % height === 0) {
-      const width = sum / height
-      if((height-2)*(width-2)===yellow){
-        return [width,height]
+  let total = brown + yellow;
+  let height = 3;
+  while (true) {
+    if (total % height === 0) {
+      let width = total / height;
+      if ((width - 2) * (height - 2) === yellow) {
+        console.log([width, height]);
+        return [width, height];
       }
     }
+    height++;
   }
 }
