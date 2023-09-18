46d4b69228351c6f2a76e04e2ffe96a32fc9258b
Seo Jeong Woo <64897060+teapotsoup@users.noreply.github.com>
2023-09-18 17:06:58 +0900

    [unrated] Title: 달리기 경주, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -1,28 +1,22 @@
 function solution(players, callings) {
+  var answer = []
   const playersObj = players.reduce((obj, player, idx) => {
     obj[player] = idx;
     return obj;
   }, {});
-
-  const sortedPlayers = players.slice().sort((a, b) => playersObj[a] - playersObj[b]);
-
   for (let i = 0; i < callings.length; i++) {
-    const calling = callings[i];
-    const currentIndex = playersObj[calling];
-    
-    if (currentIndex !== undefined && currentIndex > 0) {
+    const currentIndex = playersObj[callings[i]];
       const prevIndex = currentIndex - 1;
-      const currentPlayer = sortedPlayers[currentIndex];
-      const prevPlayer = sortedPlayers[prevIndex];
+      const currentPlayer = players[currentIndex];
+      const prevPlayer = players[prevIndex];
 
-      if (prevPlayer !== calling) {
-        sortedPlayers[currentIndex] = prevPlayer;
-        sortedPlayers[prevIndex] = currentPlayer;
-        playersObj[calling] = prevIndex;
+      if (prevPlayer !== callings[i]) {
+        players[currentIndex] = prevPlayer;
+        players[prevIndex] = currentPlayer;
+        playersObj[callings[i]] = prevIndex;
         playersObj[prevPlayer] = currentIndex;
       }
-    }
   }
-  answer = sortedPlayers
+  answer = players
   return answer;
 }
